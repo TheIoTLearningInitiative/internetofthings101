83705814b956810a3a9b52ac4f31e3e4476e9213
Abraham Arce <xe1gyq@gmail.com>
2015-07-29 15:49:23 +0000

    Core: Gpio Galileo Platform Type
    Galileo Version 2 Platform Type
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- iotpy/core/gpio.py
+++ iotpy/core/gpio.py
@@ -1,14 +1,18 @@
 import mraa
+import time
 
 class Gpio(object):
 
     def __init__(self):
-        pass
+        self.x = 0
+        if mraa.getPlatformType() == 1:
+            self.x = mraa.Gpio(12)
 
     def toggle(self):
-        x = mraa.Gpio(13)
-        x.dir(mraa.DIR_OUT)
-        value = not x.read()
-        x.write(value)
+        self.x.dir(mraa.DIR_OUT)
+        value = not self.x.read()
+        self.x.write(value)
+        time.sleep(1)
+        self.x.write(not value)
 
 # End of File
