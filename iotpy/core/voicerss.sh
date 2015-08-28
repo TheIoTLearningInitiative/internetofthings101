e0b62fe86a0b0a2ddba9e70bf8c7bbabc64fafed
Abraham Arce <xe1gyq@gmail.com>
2015-08-28 20:30:53 +0000

    Core: Voice Rss Base Code
    Voice Rss allows us to enable text to speech capabilities
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ iotpy/core/voicerss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+text=$1
+mashapekey=`cat configuration/voicerss.mk`
+apikey=`cat configuration/voicerss.ak`
+
+curl -X POST --include "https://voicerss-text-to-speech.p.mashape.com/?key=${apikey}" \
+  -H "X-Mashape-Key: ${mashapekey}" \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d 'c=mp3' \
+  -d 'f=48khz_16bit_stereo' \
+  -d 'hl=en-us' \
+  -d 'r=0' \
+  -d "src=${text}" > voicerss.sound
+
+mpg123 voicerss.sound
+
+# End of File
