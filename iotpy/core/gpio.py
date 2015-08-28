02be835564a064e49a65d0dd757a86c45b4ab7aa
xe1gyq@gmail.com <Abraham Arce Moreno>
2015-08-28 07:01:45 +0000

    IoTPy: Gpio Edison Pin
    Edison pin shall be initialized
    
    Signed-off-by: xe1gyq@gmail.com <Abraham Arce Moreno>

--- iotpy/core/gpio.py
+++ iotpy/core/gpio.py
@@ -7,6 +7,8 @@ class Gpio(object):
         self.x = 0
         if mraa.getPlatformType() == 1:
             self.x = mraa.Gpio(12)
+        if mraa.getPlatformType() == 2:
+            self.x = mraa.Gpio(13)
 
     def toggle(self):
         self.x.dir(mraa.DIR_OUT)
