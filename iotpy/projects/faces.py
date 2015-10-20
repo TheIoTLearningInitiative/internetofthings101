6f542e28d482de261b088124f3ea9a9ff132f514
Abraham Arce <xe1gyq@gmail.com>
2015-10-20 04:13:42 +0000

    Projects: Faces Base Code
    Base code for Faces project
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ iotpy/projects/faces.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import cv2
+
+from libraries.picture import Picture
+from libraries.tweet import twythonTimelineSet
+
+class Faces(object):
+
+    def __init__(self):
+        self.picture = Picture()
+        self.imageinput = None
+        self.cascPath = "libraries/haarcascade_frontalface_alt.xml"
+        self.imageoutput = "output/out.jpeg"
+
+    def execute(self):
+        faceCascade = cv2.CascadeClassifier(self.cascPath)
+        image = cv2.imread(self.imageinput)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+        print "Found {0} faces!".format(len(faces))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imwrite(self.imageoutput, image)
+        cv2.waitKey(0)
+
+    def share(self):
+        self.picture.capture()
+        self.imageinput = self.picture.path()
+        self.execute()
+        #twythonTimelineSet("#IoT #IoTLearningInit #IoTLearningInitiative IoTPy Selfie Project!", picturepath)
+
+# End of File
