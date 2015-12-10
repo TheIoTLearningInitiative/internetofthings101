5f24dca6665241b9ff94dcdc19bdba641d825230
Abraham Arce <xe1gyq@gmail.com>
2015-12-10 05:28:44 +0000

    Project: Pywapi Base Code
    Base code for pywapi
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- code/project/main.py
+++ code/project/main.py
@@ -2,6 +2,7 @@
 
 import psutil
 import paho.mqtt.client as paho
+import pywapi
 
 def dataNetwork():
 
@@ -17,9 +18,16 @@ if __name__ == '__main__':
     print "Hello Internet of Things 101"
     packets = dataNetwork()
     print "Packets: %d " % packets
+
     mqttclient = paho.Client()
     mqttclient.on_publish = on_publish
     mqttclient.connect("test.mosquitto.org", 1883, 60)
     mqttclient.publish("IoT101/Project", packets)
+
+    weather = pywapi.get_weather_from_yahoo('MXJO0043', 'metric')
+    message = "Reporte del Clima en " + weather['location']['city']
+    message = message + ", Temperatura " + weather['condition']['temp'] + " grados centigrados"
+    message = message + ", Presion Atmosferica " + weather['atmosphere']['pressure'] + " milibares"
+    print message
 
 # End of File
