56fd55a04c78749273aa9c2796d554b59d73f461
Abraham Arce <xe1gyq@gmail.com>
2015-12-09 22:37:05 -0600

    Project: Psutil Base Code
    Base code for psutil python library
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- code/project/main.py
+++ code/project/main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 
+import psutil
+
+def dataNetwork():
+
+    netdata = psutil.net_io_counters()
+    return netdata.packets_sent + netdata.packets_recv
+
 if __name__ == '__main__':
 
     print "Hello Internet of Things 101"
+    print "Packets: %d " % dataNetwork()
 
 # End of File
