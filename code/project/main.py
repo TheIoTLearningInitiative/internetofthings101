739c88e2773e5233d16245ca44df7906e9601765
Abraham Arce <xe1gyq@gmail.com>
2015-12-10 05:12:23 +0000

    Project: Mqtt Base Code
    Base code for Mqtt protocol
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- code/project/main.py
+++ code/project/main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
 
 import psutil
+import paho.mqtt.client as paho
 
 def dataNetwork():
 
     netdata = psutil.net_io_counters()
     return netdata.packets_sent + netdata.packets_recv
 
+def on_publish(mosq, obj, msg):
+
+    print("MQTT Message Sent!")
+
 if __name__ == '__main__':
 
     print "Hello Internet of Things 101"
-    print "Packets: %d " % dataNetwork()
+    packets = dataNetwork()
+    print "Packets: %d " % packets
+    mqttclient = paho.Client()
+    mqttclient.on_publish = on_publish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.publish("IoT101/Project", packets)
 
 # End of File
